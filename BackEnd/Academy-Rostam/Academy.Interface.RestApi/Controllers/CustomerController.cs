07b813be1e0106b1d0e5fe4b543dc13e05cc2a31
A <A@ACLIENT95>
2019-06-24 12:08:11 +0430

    backend initial :muslce:

---
+++ BackEnd/Academy-Rostam/Academy.Interface.RestApi/Controllers/CustomerController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Academy.Interface.RestApi.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Academy.Interface.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private static List<Customer> _Customers = new List<Customer>()
+        {
+            new Customer()
+            {
+                Id = 1,Firstname = "milad",Lastname = "safari"
+            },new Customer()
+            {
+                Id = 2,Firstname = "ali",Lastname = "safari"
+            }
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            return _Customers;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Customer> Get(int id)
+        {
+            return _Customers.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Customer customer)
+        {
+            if (_Customers.FirstOrDefault()==null)
+            {
+                customer.Id = 1;
+            }
+            else
+            {
+                customer.Id = _Customers.Max(x => x.Id) + 1;
+            }
+
+            _Customers?.Add(new Customer()
+            {
+                Lastname = customer.Lastname,
+                Firstname = customer.Firstname,
+                Id = customer.Id
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Customer customer)
+        {
+             var targetCustomer = _Customers.FirstOrDefault(x => x.Id == id);
+             if (targetCustomer != null)
+             {
+                 targetCustomer.Firstname = customer.Firstname;
+                 targetCustomer.Lastname = customer.Lastname;
+                 targetCustomer.Id = id;
+             }
+             else
+             {
+                 return NotFound();
+             }
+             return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var customer= _Customers.FirstOrDefault(x => x.Id == id);
+            if (customer ==null)
+            {
+                return NoContent();
+            }
+            _Customers.Remove(customer);
+            return Ok();
+        }
+    }
+}
