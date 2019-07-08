25a34a68874e9ef5c67b1d65b48e64cfd8b1733f
A <A@ACLIENT95>
2019-07-08 09:20:17 +0430

    what is problem?

--- BackEnd/Academy-Rostam/Academy.Persistence.EF/Migrations/20190707113613_addChildCourseCategory.Designer.cs
+++ BackEnd/Academy-Rostam/Academy.Persistence.EF/Migrations/20190707113613_addChildCourseCategory.Designer.cs
@@ -4,14 +4,16 @@ using Academy.Persistence.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Academy.Persistence.EF.Migrations
 {
     [DbContext(typeof(AcademyContext))]
-    partial class AcademyContextModelSnapshot : ModelSnapshot
+    [Migration("20190707113613_addChildCourseCategory")]
+    partial class addChildCourseCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BackEnd/Academy-Rostam/Academy.Persistence.EF/Migrations/20190707113613_addChildCourseCategory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Academy.Persistence.EF.Migrations
+{
+    public partial class addChildCourseCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
